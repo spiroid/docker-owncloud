e56b764496a6a942b9b9aecd8b080f2f215cef9f
Jonathan Dray <jonathan.dray@gmail.com>
2013-12-29 12:32:26 +0100

    Add owncloud autoconfiguration file for initial instance config
    Also moves data directory to /data and a volume on that location
    This fixes #1 and also resolves #2

---
+++ owncloud-postgresql/resources/var/www/owncloud/config/autoconfig.php
@@ -0,0 +1,12 @@
+<?php
+$AUTOCONFIG = array(
+    "directory"     => "/data",
+    "dbtype"        => "pgsql",
+    "dbname"        => "owncloud",
+    "dbuser"        => "owncloud",
+    "dbpass"        => "owncloud",
+    "dbhost"        => "localhost",
+    "dbtableprefix" => "",
+    "adminlogin"    => "admin",
+    "adminpass"     => "admin",
+);
